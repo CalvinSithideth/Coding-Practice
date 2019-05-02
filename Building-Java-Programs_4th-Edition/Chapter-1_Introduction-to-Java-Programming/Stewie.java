a9d659a69841f4d0852d8db22df77df571406011
Calvin <sithideth6540@students.cptc.edu>
2019-05-02 13:08:25 -0700

    First commit; Setup folders; Added Ex 1.1 - Stewie.java

---
+++ Building-Java-Programs_4th-Edition/Chapter-1_Introduction-to-Java-Programming/Stewie.java
@@ -0,0 +1,18 @@
+/*
+BJP4 Exercise 1.1: Stewie
+Assignment: 
+Write a complete Java program in a class named Stewie that prints the following output:
+
+//////////////////////
+|| Victory is mine! ||
+\\\\\\\\\\\\\\\\\\\\\\
+
+*/
+
+public class Stewie {
+    public static void main( String[] args ) {
+        System.out.println("//////////////////////");
+        System.out.println("|| Victory is mine! ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
