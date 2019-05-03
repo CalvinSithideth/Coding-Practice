366d1055262d5bd4888bec89eb406eb64851d65e
CalvinSithideth <calvin.sithideth@gmail.com>
2019-05-03 10:47:22 -0700

    Added BJP4 Exercise 1.2: Spikey

---
+++ Building-Java-Programs_4th-Edition/Chapter-1_Introduction-to-Java-Programming/Spikey.java
@@ -0,0 +1,24 @@
+/*
+BJP4 Exercise 1.2: Spikey
+Assignment: 
+Write a complete Java program in a class named Spikey that prints the following output:
+
+  \/
+ \\//
+\\\///
+///\\\
+ //\\
+  /\
+
+*/
+
+public class Spikey {
+    public static void main(String[] args) {
+        System.out.println("  \\/");
+        System.out.println(" \\\\//");
+        System.out.println("\\\\\\///");
+        System.out.println("///\\\\\\");
+        System.out.println(" //\\\\");
+        System.out.println("  /\\");
+    }
+}
