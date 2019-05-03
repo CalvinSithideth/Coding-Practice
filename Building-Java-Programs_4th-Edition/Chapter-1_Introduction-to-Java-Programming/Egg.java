3d5b3eafbc750641f4eefb9906d7de67cd373200
CalvinSithideth <calvin.sithideth@gmail.com>
2019-05-03 13:13:40 -0700

    Added BJP4 Exercise 1.9: Egg

---
+++ Building-Java-Programs_4th-Edition/Chapter-1_Introduction-to-Java-Programming/Egg.java
@@ -0,0 +1,28 @@
+/*
+BJP4 Exercise 1.9: Egg
+Assignment: 
+Write a complete Java program in a class named Egg that displays the following output:
+
+
+  _______
+ /       \
+/         \
+-"-'-"-'-"-
+\         /
+ \_______/
+
+
+*/
+
+public class Egg {
+
+    public static void main( String[] args ) {
+        System.out.println("  _______");
+        System.out.println(" /       \\");
+        System.out.println("/         \\");
+        System.out.println("-\"-'-\"-'-\"-");
+        System.out.println("\\         /");
+        System.out.println(" \\_______/");
+    }
+
+}
