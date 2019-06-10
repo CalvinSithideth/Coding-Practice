9424ddca45a2ba5fdeb0eae271247ea4d412c6ed
CALVIN Sithideth <sithideth6540@students.cptc.edu>
2019-06-10 11:38:15 -0700

    Fixed "powers of" typo in instructions

--- Practice-It/Building-Java-Programs_4th-Edition/Chapter-12_Recursion/E-12.1_starString.java
+++ Practice-It/Building-Java-Programs_4th-Edition/Chapter-12_Recursion/E-12.1_starString.java
@@ -7,11 +7,11 @@ returns a string of stars (asterisks) 2n long (i.e., 2 to the nth power).
 
 For example:
 Call            Output              Reason
-starString(0);	"*"                 20 = 1
-starString(1);	"**"                21 = 2
-starString(2);	"****"              22 = 4
-starString(3);	"********"          23 = 8
-starString(4);	"****************"	24 = 16
+starString(0);	"*"                 2^0 = 1
+starString(1);	"**"                2^1 = 2
+starString(2);	"****"              2^2 = 4
+starString(3);	"********"          2^3 = 8
+starString(4);	"****************"	2^4 = 16
 
 You should throw an IllegalArgumentException if passed a value less than 0.
 
